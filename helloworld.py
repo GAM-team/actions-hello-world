2b844b539b049e7b65f017c7dcae2c5468d18cd7
Jay Lee <jay0lee@gmail.com>
2021-10-05 15:24:34 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,13 @@
+import os
 from platform import python_version
 from ssl import SSL_VERSION
 
-print(f'Python {python_version()} {OPENSSL_VERSION}
+if os.environ.get('STATICX_PROG_PATH', False):
+    execution_type = 'staticx'
+elif getattr(sys, 'frozen', False):
+    execution_type = 'pyinstaller'
+else:
+    execution_type 'pythonsource'
+
+print(f'Python {python_version()} {OPENSSL_VERSION}')
+print(f'Execution type: {execution_type}')
