dbb37a6fa464a287dd043d27f8e1a718aeb912d1
Jay Lee <jay0lee@gmail.com>
2023-04-02 15:43:23 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ import google.auth.transport.requests
 import requests
 from google.auth.transport.requests import AuthorizedSession
 
-creds, project_id = google.auth.default(scopes='https://www.googleapis.com/auth/cloud-platform')
+creds, project_id = google.auth.default(scopes='https://www.googleapis.com/auth/iam')
 request = google.auth.transport.requests.Request()
 creds.refresh(request)
 authed_session = AuthorizedSession(creds)
