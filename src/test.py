c1e868b41fe0d8b3ada25d2b9e5765c474b64e18
Jay Lee <jay0lee@gmail.com>
2023-09-06 10:53:29 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
+import json
 import google.auth
 import google.auth.transport.requests
 import requests
@@ -9,5 +10,9 @@ creds.refresh(request)
 authed_session = AuthorizedSession(creds)
 
 resp = authed_session.get('https://oauth2.googleapis.com/tokeninfo')
-print(resp.json())
+print('About my Google access:')
+print(json.dumps(resp.json(), indent=2))
 
+resp2 = authed_session.get('https://cloudresourcemanager.googleapis.com/v3/projects')
+print('Projects I can see:')
+print(json.dumps(resp2.json(), indent=2))
