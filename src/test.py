fd70d9650563fc72864e6cf45a07f030743c7952
Jay Lee <jay0lee@gmail.com>
2023-09-06 10:46:24 -0400

    cloud-platform scope

--- src/test.py
+++ src/test.py
@@ -3,10 +3,11 @@ import google.auth.transport.requests
 import requests
 from google.auth.transport.requests import AuthorizedSession
 
-creds, project_id = google.auth.default(scopes='https://www.googleapis.com/auth/iam')
+creds, project_id = google.auth.default(scopes='https://www.googleapis.com/auth/cloud-platform')
 request = google.auth.transport.requests.Request()
 creds.refresh(request)
 authed_session = AuthorizedSession(creds)
 
 resp = authed_session.get('https://oauth2.googleapis.com/tokeninfo')
 print(resp.json())
+
