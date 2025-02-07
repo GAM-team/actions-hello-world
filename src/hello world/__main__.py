2bfda659ab5042ba46f39e42216da3e11685d559
Jay Lee <jay0lee@gmail.com>
2025-02-07 07:11:47 -0500

    [no ci] Create __main__.py

---
+++ src/hello world/__main__.py
@@ -0,0 +1,9 @@
+import requests
+
+def main():
+  resp = requests.head('https://www.google.como')
+  date = resp.headers.get('date', 'Unknown')
+  print(f'Hello world, it is {date}')
+
+if __name__ == '__main__':
+  main()
