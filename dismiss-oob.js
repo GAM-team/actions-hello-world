c18d4a8383d01dbf1e2791c8f896459e8d49374b
Jay Lee <jay0lee@gmail.com>
2025-08-26 11:07:13 -0400

    [no ci] Create dismiss-oob.js

---
+++ dismiss-oob.js
@@ -0,0 +1,37 @@
+const wdio = require("webdriverio");
+const { Key } = require('webdriverio');
+
+async function runSSD() {
+    const opts = {
+        port: 4723,
+        logLevel: "silent",
+        capabilities: {
+            platformName: "Windows",
+            "appium:app": "C:\\Windows\\System32\\notepad.exe",
+            "appium:automationName": "Windows",
+        },
+    };
+
+    let driver;
+    try {
+        driver = await wdio.remote(opts);
+        await driver.saveScreenshot("np0.png");
+        await driver.sendKeys([Key.Enter]);
+        await driver.saveScreenshot("np1.png");
+        await driver.sendKeys([Key.Enter]);
+        await driver.saveScreenshot("np2.png");
+        await driver.sendKeys([Key.Esc]);
+        await driver.saveScreenshot("np3.png");
+        
+    } catch (error) {
+        console.error("Error during Notepad run:", error);
+    }
+
+     finally {
+        if (driver) {
+            await driver.deleteSession(); // Close the Appium session
+        }
+    }
+}
+
+runSSD();
