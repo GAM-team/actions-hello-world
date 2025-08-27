3f2d7561f091b2f254d55c843eb362e8b5852061
Jay Lee <jay0lee@gmail.com>
2025-08-27 06:01:59 -0400

    Update ssd.js

--- ssd.js
+++ ssd.js
@@ -83,7 +83,6 @@ async function runSSD() {
         await driver.sendKeys(id_arr);
         await driver.saveScreenshot('login02.png');
         await driver.sendKeys([Key.Tab]);
-        totp.options = { algorithm: 'sha256' }
         const token_value = totp.generate(process.env.TOTP_SECRET, {algorithm: 'sha256'})
         token_arr =  [...token_value];
         await driver.sendKeys(token_arr);
