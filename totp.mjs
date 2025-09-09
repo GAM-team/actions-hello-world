3edccbc4196a1c8cebb6842c457c0333b720ce66
Jay Lee <jay0lee@gmail.com>
2025-09-09 12:14:45 -0400

    Create totp.mjs

---
+++ totp.mjs
@@ -0,0 +1,7 @@
+import { TOTP } from 'totp-generator';
+
+const not_secret = 'OJCUKVCEKVLW6MLGOJ3XSZCVGZATCSTPPJZEKZ5ZKNTUEVZTVLQ';
+console.log('Our secret is ' + not_secret.length + ' characters.');
+const { token_value } = await TOTP.generate(not_secret, {algorithm: 'SHA-256'});
+console.log('Our token is ' + token_value.length + ' characters.');
+const token_arr =  [...token_value];
