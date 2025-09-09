bc5a46c3ee38bdf33c46fccce588e670b153addd
Jay Lee <jay0lee@gmail.com>
2025-09-09 12:19:32 -0400

    Update totp.mjs

--- totp.mjs
+++ totp.mjs
@@ -2,6 +2,8 @@ import { TOTP } from 'totp-generator';
 
 const not_secret = 'OJCUKVCEKVLW6MLGOJ3XSZCVGZATCSTPPJZEKZ5ZKNTUEVZTVLQ';
 console.log('Our secret is ' + not_secret.length + ' characters.');
-const { token_value } = TOTP.generate(not_secret, {algorithm: 'SHA-256'});
-console.log('Our token is ' + token_value.length + ' characters.');
-const token_arr =  [...token_value];
+const { otp } = TOTP.generate(not_secret, {algorithm: 'SHA-256'});
+console.log('Our token is ' + otp.length + ' characters.');
+console.log('Our otp is ' + otp);
+const otp_arr =  [...otp];
+console.log('otp_arr is ' + otp_arr);
