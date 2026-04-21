6b9d6bfae34f4cceea83b1ddda130a76e3836548
Jay Lee <jay0lee@gmail.com>
2026-04-21 10:52:51 -0400

    Enhance argument parser and error handling
    Updated argument parser description and added error handling for setting multiprocessing start method.

--- multiprocess-hw.py
+++ multiprocess-hw.py
@@ -7,7 +7,7 @@ def worker():
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    parser = argparse.ArgumentParser(description="Multiprocessing Method")
+    parser = argparse.ArgumentParser(description="Multiprocessing Method Tester")
     parser.add_argument(
         "--method", 
         choices=["spawn", "fork", "forkserver"], 
@@ -15,7 +15,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     if args.method:
-        multiprocessing.set_start_method(args.method)
+        try:
+            multiprocessing.set_start_method(args.method)
+        except RuntimeError as e:
+            print(f"Error setting method: {e}")
     print(f"Python Version: {sys.version}")
     print(f"Multiprocessing Method: {multiprocessing.get_start_method()}")
     print("-" * 30)
